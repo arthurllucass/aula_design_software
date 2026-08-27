53fb5a36dca28f6c265e0dd569390d43cc50362f
WarBlack258 <warblack4@gmail.com>
2026-08-27 19:51:45 -0300

    refatora código para melhorar a legibilidade e a formatação em várias classes
    TICKETS -
    1. Agora confirma permite apenas a ciração de novos tickets se o evento não estiver cheio
    
    2. Agora verifica se o ticket já foi utilizado antes de permitir a entrada

--- src/main/java/br/edu/eventhub/model/Venue.java
+++ src/main/java/br/edu/eventhub/model/Venue.java
@@ -1,5 +1,13 @@
 package br.edu.eventhub.model;
+
 public class Venue {
- public String id; public String name; public int capacity;
- public Venue(String id,String name,int capacity){this.id=id;this.name=name;this.capacity=capacity;}
+    public String id;
+    public String name;
+    public int capacity;
+
+    public Venue(String id, String name, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+    }
 }
